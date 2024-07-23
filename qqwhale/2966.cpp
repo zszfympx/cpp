8c3c81e6c55c8bcb0a6c20339c3588757039ff58
zszfympx <2792668017@qq.com>
2024-07-23 15:07:11 +0800

    2966

--- qqwhale/2966.cpp
+++ qqwhale/2966.cpp
@@ -1,4 +1,39 @@
 #include<bits/stdc++.h>
+#define ONLINE_JUDGE
+#ifdef ONLINE_JUDGE
+#define getchar() p1 == p2 && (p2 = (p1 = buf) + fread (buf, 1, 1 << 18, stdin), p1 == p2) ? EOF : *p1++
+#define flush() fwrite (buffer, 1, p11 + 1, stdout), p11 = -1
+#define putchar(c) p11 == p22 ? flush(), buffer[++p11] = c : buffer[++p11] = c
+char buf[1 << 18], *p1 = buf, *p2 = buf;
+char buffer[1 << 18];
+int p11 = -1;
+const int p22 = (1 << 18) - 1;
+#endif
+template <typename T> inline void rei (T &x) {
+	x = 0; bool f = 1; int ch;
+	while (!isdigit (ch = getchar ())) f = ch != '-';
+	if (f) do x = x * 10 + (ch & 15); while (isdigit (ch = getchar ()));
+	else   do x = x * 10 - (ch & 15); while (isdigit (ch = getchar ()));
+}
+template <typename T> inline void wri (T x) {
+	if (x < 0) putchar ('-'), x = ~x + 1;
+	static int stk[40], top = 0;
+	while (x) stk[++top] = x % 10, x /= 10;
+	if (top == 0) stk[++top] = 0;
+	while (top) putchar (stk[top--] ^ 48);
+	putchar (' ');
+}
+template <typename T, typename... Args> inline void rei (T &x, Args&... args) {rei (x), rei (args...);}
+template <typename T, typename... Args> inline void wri (T x, Args... args) {wri (x), wri (args...);}
+template <typename T1, typename T2> inline T1 &cmin (T1 &a, const T2 &b) {return a > b ? a = b : a;}
+template <typename T1, typename T2> inline T1 &cmax (T1 &a, const T2 &b) {return a < b ? a = b : a;}
+typedef long long LL;
+typedef unsigned long long ULL;
+#define fir(i,a,b,...) for (register int i = (a), ##__VA_ARGS__; i <= (b); i++)
+#define firr(i,a,b,...) for (register int i = (a), ##__VA_ARGS__; i >= (b); i--)
+#define cls(a) memset (a, 0, sizeof a)
+#define mset(a,b) memset (a, b, sizeof a)
+#define dbug(x) (void)(cerr << #x " = " << x << endl)
 using namespace std;
 
 vector<int> vis, he, ne, to;
@@ -15,19 +50,23 @@ void dfs(int u){
     for(int i = he[u]; ~i; i = ne[i]) dfs(to[i]);
 }
 
-int main(){
-    int n, m;
-    scanf("%d%d", &n, &m);
+signed main () {
+	int n, m;
+    rei(n, m);
     vis.resize(n+1, false);
     he.resize(n+1, -1);
     for(int i = 0; i<m; i++){
         int a, b;
-        scanf("%d%d", &a, &b);
+        rei(a, b);
         add(a, b);
     }
     dfs(1);int ans = 0;
     for(int i = 1; i<=n; i++){
         if(vis[i]) ans++;
     }
-    printf("%d", ans);
+    wri(ans);
+#ifdef ONLINE_JUDGE
+	flush ();
+#endif
+	return 0;
 }
